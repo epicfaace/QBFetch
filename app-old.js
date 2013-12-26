15b359d082ee33e2ef1f8d4b060439e2956c8ed9
epicfaace <aramaswamis@gmail.com>
2013-12-26 13:21:10 -0500

    orig commit

---
+++ app-old.js
@@ -0,0 +1,97 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(90);
+var currentdate=new Date();
+var lastUpdate=  currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+function handler (req, res) {
+	var url=req.url;
+	var html=false;
+	var routes=["/","/chat","/pong","/3dgame","/fetch"];
+	var fileNames=["./index.html","./drawChat.html","./pong.html","./3dgame.html","./fetch.html"];
+	if (routes.indexOf(url) != -1) { //found in array of fileNames
+		url=fileNames[routes.indexOf(url)];
+		html=true;
+	}
+	else {
+		url="."+url;
+	}
+	fs.readFile(url,/*"utf8",*/ function (err, data) { //utf8 soporta tildes - o ¿no?
+		//res.charset="utf8";
+		if (err) {
+			res.writeHead(500);
+			//console.log(err);
+			var final = String(err);
+			if (html) final='<p>'+lastUpdate+'</p>'+final;
+			return res.end(final);
+		}
+		res.writeHead(200);
+		var final = data;
+		if (html) final='<p>'+lastUpdate+'</p>'+final;
+		res.end(data);
+	});
+}
+//app.get("/fetchEngine")
+
+var allData=[];
+
+var chatHandler=io.of('/chatHandler')
+.on('connection', function (socket) {
+/*	socket.emit goes to custom handler, socket.send goes to 'message' handler
+	socket.emit (or send) goes to only the user who sent the message;
+		chatHandler (or var name) .emit/send goes to all users
+*/
+	for (x in allData) {
+		chatHandler.emit('image',allData[x]);
+	}
+
+	socket.on('message',function(data) {
+		chatHandler.send(data);
+	});
+	socket.on('image',function(data) {
+		allData.push(data);
+		chatHandler.emit('image',data);
+	});
+	socket.on('eraseAll',function(data) {
+		allData=[];
+		chatHandler.emit('eraseAll',data);
+	});
+  //socket.on('')
+});
+
+var pongHandler=io.of('/pongHandler')
+.on('connection', function (socket) {
+/*	socket.emit goes to custom handler, socket.send goes to 'message' handler
+	socket.emit (or send) goes to only the user who sent the message;
+		chatHandler (or var name) .emit/send goes to all users
+*/
+	//socket.send('welcome!');
+
+	socket.on('message',function(data) {
+		socket.send(data);
+	});
+	socket.on('movement',function(data) {
+		socket.broadcast.emit('movement',data); //change(d) to broadcast
+	});
+  //socket.on('')
+});
+var threedgameHandler=io.of('/3dgameHandler')
+.on('connection', function (socket) {
+/*	socket.emit goes to custom handler, socket.send goes to 'message' handler
+	socket.emit (or send) goes to only the user who sent the message;
+		chatHandler (or var name) .emit/send goes to all users
+*/
+	//socket.send('welcome!');
+
+	socket.on('message',function(data) {
+		socket.send(data);
+	});
+	socket.on('playerPos',function(data) {
+		socket.broadcast.emit('playerPos',{x:data.x,y:data.y,z:data.z}); //change(d) to broadcast
+	});
+  //socket.on('')
+});
--- app.js
+++ app.js
@@ -1,96 +1,69 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var express = require('express');
+var app     = express();
+var fs = require('fs');
+var url = require('url');
+var http = require('http');
+var sys=require('sys');
 
-app.listen(90);
-var currentdate=new Date();
-var lastUpdate=  currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-function handler (req, res) {
-	var url=req.url;
-	var html=false;
-	var routes=["/","/chat","/pong","/3dgame"];
-	var fileNames=["./index.html","./drawChat.html","./pong.html","./3dgame.html"];
-	if (routes.indexOf(url) != -1) { //found in array of fileNames
-		url=fileNames[routes.indexOf(url)];
-		html=true;
-	}
-	else {
-		url="."+url;
-	}
-	fs.readFile(url,/*"utf8",*/ function (err, data) { //utf8 soporta tildes - o ¿no?
-		//res.charset="utf8";
-		if (err) {
-			res.writeHead(500);
-			//console.log(err);
-			var final = String(err);
-			if (html) final='<p>'+lastUpdate+'</p>'+final;
-			return res.end(final);
-		}
-		res.writeHead(200);
-		var final = data;
-		if (html) final='<p>'+lastUpdate+'</p>'+final;
-		res.end(data);
+app.use(express.bodyParser());
+
+app.get('/', function(req, res) {
+	res.sendfile("fetch.html");
+});
+app.get('/jquery.min.js', function(req, res) {
+	res.sendfile("jquery.min.js");
+});
+app.get('/fetchCss.css', function(req, res) {
+	res.sendfile("fetchCss.css");
+});
+app.get('/handlebars-v1.2.0.js',function(req,res) {
+	res.sendfile("handlebars-v1.2.0.js");
+});
+
+app.get('/fetchEngine', function(request, response) {
+	console.log('query received')
+	/*var query=url.parse(request.url, true).query;
+	console.log(query['info']);*/
+	var query=url.parse(request.url, false).query;
+  //res.send('You sent the name "' + query + '".');
+  console.log(query);
+	var options = {
+	  host: 'www.quinterest.org',
+	  port: 80,
+	  path: '/php/search.php?'+query+'&categ=All&difficulty=HS&tournamentyear=All',
+	};
+	http.get(options, function(result) {
+		//console.log('STATUS: ' + result.statusCode);
+		//console.log('HEADERS: ' + JSON.stringify(result.headers));
+		result.setEncoding('utf8');
+		result.on('data', function (chunk) {
+			//console.log('BODY: ' + chunk);
+			response.write(chunk);
+		});
+		result.on('end', function () {
+		    response.end();
+		    console.log("query ended");
+		});
+	}).on('error',function(err) {
+		console.log(err.message);
+		response.send(err);
 	});
+
+});
+
+app.listen(8080, function() {
+  console.log('Server is now running! Huehuehue!');
+});
+
+function checkSite(url) {
+    var site = http.createClient(80, url);
+    site.on('error', function(err) {
+        sys.debug('unable to connect to ' + url);
+        console.log(err);
+    });
+    var request = site.request('GET', '/', {'host': url});
+    request.end();
+    request.on('response', function(res) {
+        sys.debug('status code: ' + res.statusCode);
+    });
 }
-
-var allData=[];
-
-var chatHandler=io.of('/chatHandler')
-.on('connection', function (socket) {
-/*	socket.emit goes to custom handler, socket.send goes to 'message' handler
-	socket.emit (or send) goes to only the user who sent the message;
-		chatHandler (or var name) .emit/send goes to all users
-*/
-	for (x in allData) {
-		chatHandler.emit('image',allData[x]);
-	}
-
-	socket.on('message',function(data) {
-		chatHandler.send(data);
-	});
-	socket.on('image',function(data) {
-		allData.push(data);
-		chatHandler.emit('image',data);
-	});
-	socket.on('eraseAll',function(data) {
-		allData=[];
-		chatHandler.emit('eraseAll',data);
-	});
-  //socket.on('')
-});
-
-var pongHandler=io.of('/pongHandler')
-.on('connection', function (socket) {
-/*	socket.emit goes to custom handler, socket.send goes to 'message' handler
-	socket.emit (or send) goes to only the user who sent the message;
-		chatHandler (or var name) .emit/send goes to all users
-*/
-	//socket.send('welcome!');
-
-	socket.on('message',function(data) {
-		socket.send(data);
-	});
-	socket.on('movement',function(data) {
-		socket.broadcast.emit('movement',data); //change(d) to broadcast
-	});
-  //socket.on('')
-});
-var threedgameHandler=io.of('/3dgameHandler')
-.on('connection', function (socket) {
-/*	socket.emit goes to custom handler, socket.send goes to 'message' handler
-	socket.emit (or send) goes to only the user who sent the message;
-		chatHandler (or var name) .emit/send goes to all users
-*/
-	//socket.send('welcome!');
-
-	socket.on('message',function(data) {
-		socket.send(data);
-	});
-	socket.on('playerPos',function(data) {
-		socket.broadcast.emit('playerPos',{x:data.x,y:data.y,z:data.z}); //change(d) to broadcast
-	});
-  //socket.on('')
-});
